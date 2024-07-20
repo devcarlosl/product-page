398eec8ec6282cce141ba5968883b5f9c9688b8c
Carlos Lopes <dev.carloslopes@gmail.com>
2024-07-20 00:17:09 -0300

    fix: ajusta erro de importação

--- src/hooks/useCarouselHandlers/index.ts
+++ src/hooks/useCarouselHandlers/index.ts
@@ -1,7 +1,5 @@
 import { useState } from "react";
 
-import { FieldProductDetailsByColor } from "@/types/query";
-
 export const useCarouselHandlers = (
   images: FieldProductDetailsByColor["entity"]["fieldProdDetailsBcolGallery"]
 ) => {
